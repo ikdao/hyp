886d0f6b61f600d123c391a466f13ae04403ffe4
Ikdao <30446186+ikdao@users.noreply.github.com>
2025-10-04 03:43:10 +0545

    Update hyp.js

--- hyp.js
+++ hyp.js
@@ -35,6 +35,65 @@ export const h = (ty, prp, ...chd) => {
   return{ ty, prp, chd: flatChildren, key: prp.key || null, ref: prp.ref || null};
 };
 
+// --- Class Modules
+export const c = (base = Object, def) => {
+  return class Module extends base {
+    constructor(props) {
+      super(props);
+
+      this.props = props || {};
+      this.state = def.state ? def.state() : {};
+      this.render = def.render.bind(this);
+
+      this.effects = [];
+      this.cleanups = [];
+
+      if (typeof def.setup === "function") {
+        def.setup.call(this, props);
+      }
+
+      if (typeof this.componentWillMount === "function") {
+        this.componentWillMount();
+      }
+    }
+
+    setState(partial) {
+      this.state = { ...this.state, ...partial };
+      s.add(() => this.update());
+    }
+
+    mount(container) {
+      if (typeof this.componentDidMount === "function") {
+        s.add(() => this.componentDidMount());
+      }
+      const vnode = this.render(this.props, this.state, this.props.children);
+      this._vnode = vnode;
+      return vnode;
+    }
+
+    update() {
+      if (typeof this.componentDidUpdate === "function") {
+        this.componentDidUpdate(this.props, this.state);
+      }
+      const newVNode = this.render(this.props, this.state, this.props.children);
+      this._vnode = newVNode;
+      return newVNode;
+    }
+
+    onMount(fn) { this.effects.push(fn); }
+    runEffects() { this.effects.forEach(fn => fn()); }
+
+    onUnmount(fn) { this.cleanups.push(fn); }
+    cleanup() {
+      if (typeof this.componentWillUnmount === "function") {
+        this.componentWillUnmount();
+      }
+      this.cleanups.forEach(fn => fn());
+    }
+  };
+};
+
+
 // --- 1. Scheduler (s) ---
 export const s = (function() {
   const pe = new Set();
@@ -137,10 +196,24 @@ export const e = (vnode, container) => {
     const oldoID = oID;
     oID = iD;
     o.get(iD).ei = 0;
+
+    // --- If it's an m() Module class ---
+    if (fn.prototype && fn.prototype.render) {
+      if (!vN.inst) {
+        vN.inst = new fn(vN.prp || {});
+        vN.inst._vnode = vN;
+      }
+      const out = vN.inst.render(vN.prp || {}, vN.inst.state, vN.prp?.children);
+      oID = oldoID;
+      return out;
+    }
+
+    // --- Function component ---
     const rN = fn(vN.prp || {});
     oID = oldoID;
     return rN;
   };
+
   const setprp = (el, key, val) => {
     if (key === 'key' || key === 'ref') return;
     if (key.startsWith('on') && typeof val === 'function')
@@ -156,145 +229,176 @@ export const e = (vnode, container) => {
       val == null || val === false ? el.removeAttribute(key) : el.setAttribute(key, val === true ? '' : val);
     }
   };
+
   const removeprp = (el, key, val) => {
     if (key.startsWith('on') && typeof val === 'function') {
       el.removeEventListener(key.slice(2).toLowerCase(), val);
     } else setprp(el, key, null);
   };
+
   const unmount = (vnode) => {
     if (!vnode?.coI) return;
     const ctx = o.get(vnode.coI);
     if (ctx) {
+      // If it was a Module, call cleanup
+      if (vnode.inst && typeof vnode.inst.cleanup === "function") {
+        vnode.inst.cleanup();
+      }
       ctx.hk.forEach(h => { if (h && typeof h.cleanup === 'function') h.cleanup(); });
       o.delete(vnode.coI);
     }
     vnode.chd?.forEach(unmount);
   };
-const SVG_NS = "http://www.w3.org/2000/svg";
-const createDOM = (vnode, inSVG = false) => {
-  if (typeof vnode !== 'object' || vnode == null) return document.createTextNode(String(vnode));
-  if (Array.isArray(vnode)) {
-    const frag = document.createDocumentFragment();
-    vnode.forEach(c => c != null && frag.appendChild(createDOM(c, inSVG)));
-    return frag;
-  }
-  const { ty, prp, chd, ref } = vnode;
-  if (typeof ty === 'function') {
-    const rN = wC(vnode);
-    vnode.chd = [rN];
-    return createDOM(rN, inSVG);
-  }
-  const isSVG = inSVG || ty === 'svg';
-  const el = isSVG
-    ? document.createElementNS(SVG_NS, ty)
-    : document.createElement(ty);
-  if (prp) Object.keys(prp).forEach(k => {
-    if (k.startsWith('on') && typeof prp[k] === 'function') {
-      el.addEventListener(k.slice(2).toLowerCase(), prp[k]);
-    } else if (isSVG) {
-      el.setAttribute(k, prp[k]);
-    } else {
-      setprp(el, k, prp[k]);
-    }
-  });
-  if (ref) typeof ref === 'function' ? ref(el) : ref.current = el;
-  chd?.forEach(c => el.appendChild(createDOM(c, isSVG)));
-  return el;
-};
 
- const patch = (dom, oldV, newV) => {
-  if (typeof oldV !== 'object' || oldV == null ||
-      typeof newV !== 'object' || newV == null ||
-      oldV.ty !== newV.ty) {
-    if (oldV !== newV || oldV.ty !== newV.ty) {
-      unmount(oldV);
-      const newDom = createDOM(newV);
-      dom.replaceWith(newDom);
-      return newDom;
+  const SVG_NS = "http://www.w3.org/2000/svg";
+  const createDOM = (vnode, inSVG = false) => {
+    if (typeof vnode !== 'object' || vnode == null) return document.createTextNode(String(vnode));
+    if (Array.isArray(vnode)) {
+      const frag = document.createDocumentFragment();
+      vnode.forEach(c => c != null && frag.appendChild(createDOM(c, inSVG)));
+      return frag;
     }
+    const { ty, prp, chd, ref } = vnode;
+
+    // --- Modules ---
+    if (ty.prototype && ty.prototype.render) {
+      if (!vnode.inst) {
+        vnode.inst = new ty(prp || {});
+        vnode.inst._vnode = vnode;
+        vnode.inst.componentWillMount?.();
+      }
+      const subTree = vnode.inst.render(prp || {}, vnode.inst.state, prp?.children);
+      const dom = createDOM(subTree, inSVG);
+      vnode.dom = dom;
+      s.add(() => vnode.inst.componentDidMount?.());
+      return dom;
+    }
+
+    // --- Function components ---
+    if (typeof ty === 'function') {
+      const rN = wC(vnode);
+      vnode.chd = [rN];
+      return createDOM(rN, inSVG);
+    }
+
+    // --- Normal DOM nodes ---
+    const isSVG = inSVG || ty === 'svg';
+    const el = isSVG
+      ? document.createElementNS(SVG_NS, ty)
+      : document.createElement(ty);
+
+    if (prp) Object.keys(prp).forEach(k => {
+      if (k.startsWith('on') && typeof prp[k] === 'function') {
+        el.addEventListener(k.slice(2).toLowerCase(), prp[k]);
+      } else if (isSVG) {
+        el.setAttribute(k, prp[k]);
+      } else {
+        setprp(el, k, prp[k]);
+      }
+    });
+
+    if (ref) typeof ref === 'function' ? ref(el) : ref.current = el;
+    chd?.forEach(c => el.appendChild(createDOM(c, isSVG)));
+    return el;
+  };
+
+  const patch = (dom, oldV, newV) => {
+    if (typeof oldV !== 'object' || oldV == null ||
+        typeof newV !== 'object' || newV == null ||
+        oldV.ty !== newV.ty) {
+      if (oldV !== newV || oldV.ty !== newV.ty) {
+        unmount(oldV);
+        const newDom = createDOM(newV);
+        dom.replaceWith(newDom);
+        return newDom;
+      }
+      return dom;
+    }
+
+    // --- Module diff ---
+    if (newV.ty.prototype && newV.ty.prototype.render) {
+      const inst = oldV.inst || new newV.ty(newV.prp || {});
+      newV.inst = inst;
+      inst.props = newV.prp || {};
+      const newSubTree = inst.render(inst.props, inst.state, inst.props.children);
+      inst.componentDidUpdate?.(oldV.prp, inst.state);
+      return patch(dom, oldV.chd[0], newSubTree);
+    }
+
+    // --- Function component diff ---
+    if (typeof newV.ty === 'function') {
+      const resultVNode = wC(newV);
+      newV.chd = [resultVNode];
+      return patch(dom, oldV.chd[0], resultVNode);
+    }
+
+    // --- DOM node diff ---
+    const oldprp = oldV.prp || {};
+    const newprp = newV.prp || {};
+    Object.keys(oldprp).forEach(k => { if (!(k in newprp)) removeprp(dom, k, oldprp[k]); });
+    Object.keys(newprp).forEach(k => {
+      const oldVal = oldprp[k];
+      const newVal = newprp[k];
+      if (oldVal !== newVal) {
+        if (k === 'value' && (dom.tagName === 'INPUT' || dom.tagName === 'TEXTAREA')) {
+          if (dom.value !== newVal) dom.value = newVal;
+          return;
+        }
+        if (k === 'checked' && dom.tagName === 'INPUT') {
+          if (dom.checked !== newVal) dom.checked = newVal;
+          return;
+        }
+        if (k.startsWith('on') && typeof oldVal === 'function') dom.removeEventListener(k.slice(2).toLowerCase(), oldVal);
+        setprp(dom, k, newVal);
+      }
+    });
+
+    // --- Hybrid child diff ---
+    const oldChildren = oldV.chd || [];
+    const newChildren = newV.chd || [];
+
+    const oldKeyed = new Map();
+    oldChildren.forEach((c, i) => {
+      if (c?.prp?.key != null) oldKeyed.set(c.prp.key, { vnode: c, index: i });
+    });
+
+    const max = Math.max(oldChildren.length, newChildren.length);
+    let domIndex = 0;
+
+    for (let i = 0; i < max; i++) {
+      const newC = newChildren[i];
+      const oldC = newC?.prp?.key != null ? oldKeyed.get(newC.prp.key)?.vnode : oldChildren[domIndex];
+
+      if (!oldC && newC) {
+        const newNode = createDOM(newC);
+        dom.appendChild(newNode);
+        domIndex++;
+      } else if (!newC && oldC) {
+        const domChild = dom.childNodes[domIndex];
+        unmount(oldC);
+        dom.removeChild(domChild);
+      } else if (oldC && newC) {
+        const domChild = dom.childNodes[domIndex];
+        patch(domChild, oldC, newC);
+        domIndex++;
+        if (newC.prp?.key != null) oldKeyed.delete(newC.prp.key);
+      }
+    }
+
+    oldKeyed.forEach(({ vnode, index }) => {
+      const domChild = dom.childNodes[index];
+      if (domChild) {
+        unmount(vnode);
+        dom.removeChild(domChild);
+      }
+    });
+
     return dom;
-  }
-
-  if (typeof newV.ty === 'function') {
-    const resultVNode = wC(newV);
-    newV.chd = [resultVNode];
-    return patch(dom, oldV.chd[0], resultVNode);
-  }
-
-  // --- Update prps ---
-  const oldprp = oldV.prp || {};
-  const newprp = newV.prp || {};
-  Object.keys(oldprp).forEach(k => { if (!(k in newprp)) removeprp(dom, k, oldprp[k]); });
-  Object.keys(newprp).forEach(k => {
-    const oldVal = oldprp[k];
-    const newVal = newprp[k];
-    if (oldVal !== newVal) {
-      if (k === 'value' && (dom.tagName === 'INPUT' || dom.tagName === 'TEXTAREA')) {
-        if (dom.value !== newVal) dom.value = newVal;
-        return;
-      }
-      if (k === 'checked' && dom.tagName === 'INPUT') {
-        if (dom.checked !== newVal) dom.checked = newVal;
-        return;
-      }
-      if (k.startsWith('on') && typeof oldVal === 'function') dom.removeEventListener(k.slice(2).toLowerCase(), oldVal);
-      setprp(dom, k, newVal);
-    }
-  });
-
-  // --- Hybrid key/index diffing for children ---
-  const oldChildren = oldV.chd || [];
-  const newChildren = newV.chd || [];
-
-  // Build map of old children by key
-  const oldKeyed = new Map();
-  oldChildren.forEach((c, i) => {
-    if (c?.prp?.key != null) oldKeyed.set(c.prp.key, { vnode: c, index: i });
-  });
-
-  const max = Math.max(oldChildren.length, newChildren.length);
-  let domIndex = 0;
-
-  for (let i = 0; i < max; i++) {
-    const newC = newChildren[i];
-    const oldC = newC?.prp?.key != null ? oldKeyed.get(newC.prp.key)?.vnode : oldChildren[domIndex];
-
-    if (!oldC && newC) {
-      // Create new DOM node
-      const newNode = createDOM(newC);
-      dom.appendChild(newNode);
-      domIndex++;
-    } else if (!newC && oldC) {
-      // Remove old DOM node
-      const domChild = dom.childNodes[domIndex];
-      unmount(oldC);
-      dom.removeChild(domChild);
-    } else if (oldC && newC) {
-      // Patch existing node
-      const domChild = dom.childNodes[domIndex];
-      patch(domChild, oldC, newC);
-      domIndex++;
-      // Remove keyed from map to avoid double-remove
-      if (newC.prp?.key != null) oldKeyed.delete(newC.prp.key);
-    }
-  }
-
-  // Remove leftover old keyed nodes not matched
-  oldKeyed.forEach(({ vnode, index }) => {
-    const domChild = dom.childNodes[index];
-    if (domChild) {
-      unmount(vnode);
-      dom.removeChild(domChild);
-    }
-  });
-
-  return dom;
-};
-
+  };
 
   const targetDom = container.firstChild;
   if (!prevVNode || !targetDom) {
-    container.textContent='';
+    container.textContent = '';
     container.appendChild(createDOM(vnode));
   } else {
     patch(targetDom, prevVNode, vnode);
@@ -303,6 +407,6 @@ const createDOM = (vnode, inSVG = false) => {
 };
 
 
-const HYP = {h,e,a,sA,dA,o,s};
+const HYP = {h,e,a,sA,dA,c,o,s};
 window.HYP = HYP;
 export default HYP;
