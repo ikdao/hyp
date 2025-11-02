6880911a4c90cdee740f7bf610bb370ebfd865aa
Ikdao <30446186+ikdao@users.noreply.github.com>
2025-11-02 19:58:38 +0530

    Refactor dA function variable names for clarity

--- dA.js
+++ dA.js
@@ -1,13 +1,12 @@
 // Derived Act dA()
-
 export const dA = (compute) => {
-  const sig = a();
-  const recompute = () => {
-    tracking = recompute;
-    const val = compute();
-    tracking = null;
-    sig.set(val);
-  };
-  recompute();
-  return sig;
+    const sig = a();
+    const recompute = () => {
+        tr = recompute;
+        const val = compute();
+        tr = null;
+        sig.set(val);
+    };
+    recompute();
+    return sig;
 };
