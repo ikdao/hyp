cb1fa45b1e6fff927ac4a207d374da6414ffc35d
Ikdao <30446186+ikdao@users.noreply.github.com>
2026-03-04 02:37:11 +0530

    Add index.js to export HYP framework modules

---
+++ index.js
@@ -0,0 +1,12 @@
+// HYP UI/UX Framework
+// Zero One Self License - 01SL
+// HYP Framework - Hemang Tewari
+
+export { HYP as default } from "/src/hyp.js";
+export { h } from "/src/h.js";
+export { e } from "/src/e.js";
+export { o } from "/src/o.js";
+export { s } from "/src/s.js";
+export { a } from "/src/a.js";
+export { r } from "/src/r.js";
+export { i } from "/src/i.js";
